3f5a16c9123805a81caea13159518b4ac41c0674
Chris <110124656+ChrisDc777@users.noreply.github.com>
2025-04-21 23:14:53 +0530

    feat.(ProtectRoutesV1, remove Vercel Analytics

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,15 @@
-import { auth } from "@/lib/auth"
+// import { auth } from "@/lib/auth"
  
-export default auth((req) => {
-  // if (!req.auth && req.nextUrl.pathname !== "/login") {
-  //   const newUrl = new URL("/login", req.nextUrl.origin)
-  //   return Response.redirect(newUrl)
-  // }
-})
+export { auth as middleware } from "@/lib/auth"
+
+// export default auth((req) => {
+//   // if (!req.auth && req.nextUrl.pathname !== "/login") {
+//   //   const newUrl = new URL("/login", req.nextUrl.origin)
+//   //   return Response.redirect(newUrl)
+//   // }
+// })
  
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/", "/products", "/((?!api|_next/static|_next/image|favicon.ico).*)"],
 }
